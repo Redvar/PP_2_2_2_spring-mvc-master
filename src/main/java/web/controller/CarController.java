493deb7d88be2b966e14d135deeea7298618e5d4
Redvar <pavel.qip@gmail.com>
2022-06-22 15:42:54 +0300

    Add CarSevice

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,33 +1,28 @@
+
 package web.controller;
 
 import model.Car;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
 
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class CarController {
-
-
-    @GetMapping(value = "/cars")
-    public String getCars(@RequestParam(value = "count", required = false, defaultValue = "5") Integer count,
-                          Model model) {
-        if (count!=null & count > 6) {
-            List<Car> carsList = new ArrayList<>();
-            CarService carService = new CarService();
-            carsList = carService.findCarsList();
-            model.addAttribute("cars", carsList.stream().limit(count).collect(Collectors.toList()));
-            return "cars";
-        } else {
-            return "/";
-        }
+    @GetMapping("/cars")
+    public String getCar(@RequestParam(value = "count", defaultValue = "5") int allCars, Model model) {
+        List<Car> list = new ArrayList<>();
+        list.add(new Car("BMW", "red", "Yellow"));
+        list.add(new Car("Toyota", "green", "Blue"));
+        list.add(new Car("Mercedes", "black", "black"));
+        list.add(new Car("Volkswagen", "grey", "green"));
+        list.add(new Car("Ford", "blue", "Yellow"));
+        list = CarService.carsCount(list, allCars);
+        model.addAttribute("list", list);
+        return "car";
     }
 }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,26 +1,12 @@
 package web.service;
 
 import model.Car;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarService {
-    public List<Car> findCarsList(){
-        List<Car> cars = new ArrayList<>();
-
-        Car car1 = new Car("Lada", "6", "yellow");
-        Car car2 = new Car("Audi", "A5", "red");
-        Car car3 = new Car("Peugeot", "406", "green");
-        Car car4 = new Car("Mercedes", "600", "blue");
-        Car car5 = new Car("Fiat", "124", "black");
-
-        cars.add(car1);
-        cars.add(car2);
-        cars.add(car3);
-        cars.add(car4);
-        cars.add(car5);
-
-        return cars;
+    public static List<Car> carsCount(List<Car> list, int number) {
+        if (number == 0 || number > 5) return list;
+        return list.stream().limit(number).collect(Collectors.toList());
     }
 }
